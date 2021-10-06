070e698dfe8bec84b3746b1c0c88b39d517aa9ff
Natnael <natnaelebuy@gmail.com>
2021-10-05 20:13:16 -0700

    update

---
+++ AnArray.java
@@ -0,0 +1,21 @@
+public class AnArray {
+    public static void main(String[] args) {
+        /// creat anarray of 5 string 
+        String[] person = new String[5];
+
+        person [0] = "Hawaz";
+        person [1] = "yordanos";
+        person [2] = "yonatan";
+        person [3] = "jose";
+        person [4] = "Gelila";
+
+        
+
+
+
+
+
+    }
+
+    
+}
